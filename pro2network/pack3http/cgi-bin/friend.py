76cd673b72845e21b36423b8826980c1001d6a43
ParkYS <dbtjdqkr12@naver.com>
2026-02-25 17:24:21 +0900

    0225 끝

---
+++ pro2network/pack3http/cgi-bin/friend.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')   
+
+import os
+import urllib.parse
+
+
+
+# --- get / post 요청 받을 때 ---------
+method = os.environ.get("REQUEST_METHOD", "GET")
+
+if method == "POST":
+    length = int(os.environ.get("CONTENT_LENGTH", 0))
+    body = sys.stdin.read(length)
+else:   # GET
+    body = os.environ.get("QUERY_STRING", "")
+
+params = urllib.parse.parse_qs(body)
+
+irum = params.get("name", [""])[0]
+junhwa = params.get("phone", [""])[0]
+gen = params.get("gen", [""])[0]
+
+print("Content-Type: text/html; charset=utf-8")
+print()
+print("""
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>world</title>
+</head> 
+<body>
+    입력한 값은 : 이름은 {0} 전화는 {1} 성별은 {2} 
+</body>
+</html>
+""".format(irum, junhwa, gen))
